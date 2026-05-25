e81945b44384d1310965830a84a7e5535d3c4d2d
Manimeldura Sandaru <s4089460@student.rmit.edu.au>
2026-05-25 18:15:48 +1000

    fix

--- src/test/java/BookIntegrationTest.java
+++ src/test/java/BookIntegrationTest.java
@@ -14,7 +14,7 @@ public class BookIntegrationTest {
                 new BookService(repository);
 
         Book book = new Book(
-                "1ABCDe",
+                "1ABCD",
                 "Clean Code",
                 5,
                 80,
